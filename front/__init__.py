46117cd3e6d1e72a566dfc6fdc96770c483cf00b
Marusoftware <marusoftware@outlook.jp>
2021-10-07 00:48:58 +0900

    update

--- front/__init__.py
+++ front/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app=Flask("mana_alle")
-
+app=Flask(__name__)
+print(app.template_folder)
 @app.route("/")
 def index():
-    return "<h1>このページはいつも通りゴミページです。</h1>"
+    return render_template("index.html")
 
 @app.errorhandler(404)
 def not_found(error):
